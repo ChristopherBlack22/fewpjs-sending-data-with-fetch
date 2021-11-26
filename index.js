cb7ecd6f25258155abc5e1d5fe502dc867d43c3b
Christopher <christopher.black.2209@googlemail.com>
2021-11-26 15:22:34 +0000

    commit tree e26ae65a2201e897350f2aac383d484dbf6499c0

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Add your code here
+// const formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle"
+//   };
+  
+//   const configObj = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json"
+//     },
+//     body: JSON.stringify(formData)
+//   };
+  
+//   fetch("http://localhost:3000/dogs", configObj)
+//     .then(function(response) {
+//       return response.json();
+//     })
+//     .then(function(object) {
+//       console.log(object);
+//     })
+//     .catch(function(error) {
+//         alert("Bad things! Ragnarők!");
+//         console.log(error.message);
+//     });
+
+function submitData (name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json", 
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: name, email: email
+    })
+  })
+  .then(response => response.json())
+  .then(json => document.body.innerHTML = json.id)
+  .catch(error => document.body.innerHTML = error.message)
+}
